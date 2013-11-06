bd1a9db6c557d39085a99c69da3d4dc838502caa
Christopher S. Case <chris.case@g33xnexus.com>
2013-11-05 22:43:43 -0600

    Added a setup.py file.

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pynode',
+      version='0.1.0',
+      description='A python module for building a python task worker run by node.js, providing two-way communication based on socket.io.',
+      author='Christopher S. Case',
+      author_email='chris.case@g33xnexus.com',
+      url='https://github.com/Morgul/nodepy',
+      packages=['nodepy'],
+     )
